6dcf4cc8f98e18ebd8f38ec93c3f25a7cd3925c4
thedrew5150 <andrewjphillips@gmail.com>
2024-09-13 11:31:13 -0600

    first commit

---
+++ src/app/page copy 2.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import Image from 'next/image';
+import styles from './globals.css';
+
+const Home: FC = () => (
+  <div className="container">
+    {/* Your content here */}
+  </div>
+);
+
+export default Home;
